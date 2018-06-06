b887ccdb4e211c1da2a5cfb7caaf7a1dfa549b8d
Zygimantas Matonis <zygimantas.matonis@cern.ch>
2018-06-06 17:59:28 +0200

    Unregistering service worker

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import App from "./App";
+import { unregister } from './registerServiceWorker';
+
 
 ReactDOM.render(<App/>, document.getElementById('root'));
-registerServiceWorker();
+// TODO improves performance, but makes development changes not immediate in production
+// registerServiceWorker();
+unregister();
