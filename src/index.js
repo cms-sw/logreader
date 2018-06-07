01cfb07e767df956582a6040dd543eb1f47425f6
Zygimantas Matonis <zygimantas.matonis@cern.ch>
2018-06-07 10:13:22 +0200

    Small fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import App from "./App";
 import { unregister } from './registerServiceWorker';
 
